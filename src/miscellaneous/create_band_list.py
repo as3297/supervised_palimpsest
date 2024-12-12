3f442c8c056143fe3deb83418c7691789d40ceba
Anna Starynska <as3297@rit.edu>
2024-12-12 18:16:53 -0500

    script for creating band list

---
+++ src/miscellaneous/create_band_list.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+
+
+maindir = r"c:\Data\PhD\palimpsest\Victor_data\Paris_Coislin"
+folioname = r"Par_coislin_393_054r"
+
+banddict={}
+for fname in os.listdir(os.path.join(maindir,folioname)):
+    if "tif" in fname:
+        band = fname.split("+")[1][:-4]
+        band_idx = int(band.split("_")[1])
+        banddict[band_idx]=band
+
+bands = []
+
+for idx in sorted(banddict.keys()):
+    print(idx)
+    bands.append(banddict[idx]+"\n")
+
+with open(os.path.join(maindir,"band_list.txt"),"w") as f:
+    f.writelines(bands)
