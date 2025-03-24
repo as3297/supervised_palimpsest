72dc05fd6ba79922e19c514ebbd60ee15eb62f4b
Anna Starynska <as3297@rit.edu>
2025-03-24 11:17:55 -0400

    changed patience to 15

--- src/run_training.sh
+++ src/run_training.sh
@@ -4,22 +4,22 @@
 EPOCHS=45
 BATCH_SIZE=512
 MODALITIES=("M")
-NB_NODES_IN_LAYER=512
+NB_NODES_IN_LAYER=2048
 NB_LAYERS=4
 OPTIMIZER_NAME="adam"
-LEARNING_RATE=0.00001
+LEARNING_RATE=0.0001
 DROPOUT_RATE=0.0
 LABEL_SMOOTHING=0.0
 WEIGHT_DECAY=0.0
 LOSS_NAME="sparce_categorical_crossentropy"
 MAIN_DATA_DIR="/projects/palimpsests" #"D:"
 PALIMPSEST_NAME="Verona_msXL"
-PATIENCE=-1
+PATIENCE=15
 FOLIOS_TRAIN=("msXL_335v_b" "msXL_315v_b" "msXL_318r_b" "msXL_318v_b" "msXL_319r_b" "msXL_319v_b" "msXL_322r_b" "msXL_322v_b" "msXL_323r_b" "msXL_334r_b" "msXL_334v_b" "msXL_344r_b")
 FOLIOS_VAL=("msXL_344v_b") #("msXL_315r_b")
 MODEL_DIR="/projects/supervised_palimpsest/training" #"c:\Data\PhD\ML_palimpsests\Supervised_palimpsest\training"
 LEARNING_RATE_DECAY_EPOCH_STEP=0
-CLASSES_DICT='{"undertext_renn":1,"not_undertext":0}'
+CLASSES_DICT='{"undertext":1,"not_undertext":0}'
 RESTORE_PATH="/projects/supervised_palimpsest/training/Verona_msXL/20250318-170853"
 
 
