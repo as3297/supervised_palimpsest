a47e447ad14e6754910ecfe56c8838500935adf1
Anna Starynska <as3297@rit.edu>
2025-04-15 20:58:20 -0400

    number of parallel calls in map to 1

--- src/dataset_patches_tf.py
+++ src/dataset_patches_tf.py
@@ -142,7 +142,7 @@ def build_patch_dataset_with_labels(patch_specs, window_size, band_list, padding
     ds = ds.map(
         lambda base_path, coord, label: process_patch_with_label(
             (base_path, coord, label), window_size, band_list, padding_fill),
-        num_parallel_calls=tf.data.AUTOTUNE,
+        num_parallel_calls=1,
     )
     # Cache dataset in memory after first epoch (if it fits!)
     #dataset = ds.cache() # Uncomment if dataset fits in RAM
